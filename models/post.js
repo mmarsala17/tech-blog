0f4d2cd953c45c5904885f824df94f817b00b9e3
Mark Marsala <mmarsala76@gmail.com>
2022-01-07 15:05:02 -0500

    sign-up

--- models/post.js
+++ models/post.js
@@ -20,6 +20,11 @@ Post.init(
             type: DataTypes.TEXT,
             allowNull: false,
         },
+        date: {
+            type: DataTypes.DATEONLY,
+            allowNull: false,
+            defaultValue: DataTypes.NOW
+        },
         user_id: {
             type: DataTypes.INTEGER,
             references: {
