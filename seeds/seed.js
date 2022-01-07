dd2ab765a44f7654528834344ec3712e63997250
Mark Marsala <mmarsala76@gmail.com>
2022-01-06 23:13:33 -0500

    seed data

--- seeds/seed.js
+++ seeds/seed.js
@@ -3,13 +3,17 @@ const { User, Post, Comment } = require('../models');
 
 const userData = require('./userData.json');
 
+const postData = require('./postData.json');
+
 const seedDatabase = async () => {
     await sequelize.sync({ force: true });
 
     await User.bulkCreate(userData, {
         individualHooks: true,
         returning: true,
-    })
+    });
+
+    await Post.bulkCreate(postData);
 
     process.exit(0);
 };
